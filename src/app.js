95b74fcf3b1fc5fd2cbcefaa4362d56b0541709d
DylanHedges <d.hedges1@hotmail.co.uk>
2018-05-23 07:01:16 -0400

    Added JSX elements and conditional rendering

--- src/app.js
+++ src/app.js
@@ -1,7 +1,46 @@
 console.log('app.js is running');
 
 
-var template = <h1> Random Chooser </h1>;
+var app = {
+  title: 'Main title',
+  subtitle: 'Subtitle',
+  options: ['one', 'two']
+};
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    //Display a subtitle if there is one
+    {app.subtitle && <p>{app.subtitle}</p>}
+    //If the options array has elements display 'Here are...' if it has no elements display 'No options'
+    <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>;
+    <ol>
+      <li>One</li>
+      <li>Two</li>
+    </ol>
+  </div>
+);
+
+var user = {
+  name: 'John',
+  age: 20,
+  location: 'New York'
+};
+
+function getLocation(location) {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : ' Anonymous'}</h1>
+    {(user.age && user.age >= 18) && <p>Age: {user.age}</p>}
+    {getLocation(user.location)}
+  </div>
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
